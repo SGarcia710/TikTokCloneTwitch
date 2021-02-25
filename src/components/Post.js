aa5bdcf92efbc1214ac4f5d9d2f61fdb3ac4b56e
SGarcia710 <sgarcia710@outlook.com>
2021-02-24 21:28:46 -0500

    SideBar done

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Video from 'react-native-video';
 import Foundation from 'react-native-vector-icons/Foundation';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 const Post = () => {
   return (
     <View style={styles.container}>
@@ -15,14 +20,31 @@ const Post = () => {
         repeat
       />
       <View style={styles.sideContainer}>
-        <View>
+        <TouchableOpacity style={styles.avatarContainer}>
           <Image
+            style={styles.avatar}
             source={{
               uri:
                 'https://res.cloudinary.com/sgarciacloud/image/upload/v1614154654/pexels-masha-raymers-4935657_zaxdhj.jpg',
             }}
           />
-        </View>
+          <View style={styles.plusIcon}>
+            <Entypo name="plus" size={15} color="#fff" />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.actionContainer}>
+          <MaterialCommunityIcons name="heart" size={35} color="white" />
+          <Text style={styles.actionLabel}>328.7k</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.actionContainer}>
+          <FontAwesome name="commenting" size={35} color="white" />
+          <Text style={styles.actionLabel}>342</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.actionContainer}>
+          <Fontisto name="share-a" size={35} color="white" />
+          <Text style={styles.actionLabel}>Share</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.bottomContainer}>
         <Text style={styles.username}>@sgarcia</Text>
@@ -47,6 +69,7 @@ const styles = StyleSheet.create({
   },
   sideContainer: {
     alignSelf: 'flex-end',
+    paddingRight: 7,
   },
   bottomContainer: {
     width: '100%',
@@ -70,6 +93,36 @@ const styles = StyleSheet.create({
     fontWeight: '300',
     color: '#fff',
     marginLeft: 9,
+  },
+  avatarContainer: {
+    alignItems: 'center',
+    marginBottom: 22.5,
+  },
+  avatar: {
+    width: 47,
+    height: 47,
+    borderRadius: 47 / 2,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  plusIcon: {
+    width: 21,
+    height: 21,
+    borderRadius: 21,
+    backgroundColor: '#EA4359',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -(21 / 2),
+  },
+  actionContainer: {
+    alignItems: 'center',
+    marginBottom: 22.5,
+  },
+  actionLabel: {
+    color: '#fff',
+    fontSize: 13,
+    fontWeight: '600',
+    marginTop: 5.27,
   },
 });
 
