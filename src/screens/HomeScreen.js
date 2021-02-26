84b706e8c02647b5b12a2fb87ec57c732b9de39b
SGarcia710 <sgarcia710@outlook.com>
2021-02-25 20:15:52 -0500

    Status bar now is light on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import {Post} from '../components';
 
 const HomeScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <StatusBar barStyle="light-content" />
       <Post />
     </View>
   );
